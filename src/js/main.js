4901257ce47cfe371c1bdd2f6abb52e7e897238e
Esther García <garbennes@gmail.com>
2024-01-05 18:11:12 +0100

    Add function to generate the html for each character in our character list

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,28 @@
 
 let disneyCharacters = [];
 
+// FUNCTIONS
+
+function getCharacterHtmlCode(character) {
+  let characterHtml = '';
+
+  if (typeof character.imageUrl  === 'undefined') {
+    character.imageUrl = 'https://via.placeholder.com/400x500/bbbbbb/ffffff/?text=Disney';
+  }
+
+  characterHtml += 
+  `<li class="character js__characterItem"">
+    <div class="character__image-container">
+      <img src="${character.imageUrl}" class="character__image alt="Producto: ${character.name}">
+    </div>
+    <h3 class="character__name">${character.name}</h3>
+  </li>`;
+
+  return characterHtml;
+}
+
+// CODE WHEN LOADING THE PAGE
+
 // Get data from api
 const getApiData = () => {
   fetch('//api.disneyapi.dev/character?pageSize=50')
