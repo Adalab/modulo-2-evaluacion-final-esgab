8b623f3c9dc63efa43561e42670d5e7615dc1cd4
Esther García <garbennes@gmail.com>
2024-01-05 18:08:23 +0100

    Add an array for disney characters and fetch the API to get data

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log('>> Ready :)');
+// DATA
+
+let disneyCharacters = [];
+
+// Get data from api
+const getApiData = () => {
+  fetch('//api.disneyapi.dev/character?pageSize=50')
+    .then(response => response.json())
+    .then(data => { 
+      disneyCharacters = data.data;
+      console.log(disneyCharacters);
+  });
+};
+  
+getApiData();
